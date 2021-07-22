cde5f9e67c90b599f7c4fa982ca5dd4bcfc103ad
johnPaslawski <jpaslawski93@gmail.com>
2021-07-22 15:40:19 +0200

    Implemented domain classes

---
+++ Domain/Hotel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Rating { get; set; }
+        public string Country { get; set; }
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+    }
+}
