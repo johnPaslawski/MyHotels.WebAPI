3bab26a2b0bd8ba822df3e625e5167af9391d12a
johnPaslawski <jpaslawski93@gmail.com>
2021-07-27 15:55:34 +0200

    Implemented GetAll in CountryController

---
+++ MyHotels.WebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyHotels.WebAPI.Infrastructure;
+using MyHotels.WebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHotels.WebAPI.Controllers
+{
+    [ApiController]
+    // teraz ścieżkja zostanie DOGENEROWANA na podstawie pierwszej części nazwy kontrolera
+    [Route("api/[controller]")]
+    public class CountriesController : Controller
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CountriesController> _logger;
+
+        public CountriesController(IUnitOfWork uow, IMapper mapper, ILogger<CountriesController> logger)
+        {
+            this._uow = uow;
+            this._mapper = mapper;
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        //jakie statusy chcemy zwrócić
+        // musimy zdefiniować czego klient może sie spodziewać w odpowiedzi 
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IList<CountryDto>>> GetCountries()
+        {
+            _logger.LogInformation($"{nameof(GetCountries)} called...");
+
+            try
+            {
+                var countries = await _uow.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDto>>(countries);
+
+                return Ok(results);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Something went wrong in {nameof(GetCountries)}");
+
+                // jeżeli problem nie jest dostępny w postaci standardowej metody,
+                // sami dodajemy info użytkownika:
+                //return StatusCode(StatusCodes.Status500InternalServerError,
+                //    "Internal server error, please try again later...");
+
+                return Problem("Diffrent server error, please try again later OR FIX IT...");
+            }
+        }
+    }
+}
